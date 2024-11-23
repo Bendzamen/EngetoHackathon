cfdfccd6bb1311464006e007778cfb50ee430729
Vojtěch Oram <vojtech@leadspicker.com>
2024-11-23 12:46:42 +0100

    updated loan with refinancing

--- loan_with_refinancing.py
+++ loan_with_refinancing.py
@@ -1,64 +1,89 @@
-from decimal import Decimal
+from collections import namedtuple
 
 from mortgage import Loan
-from mortgage.loan import Installment
-
-hypo_total = 100_000
-
-p_a = 0.02
-years = 2
-refinancing_year = 1
 
 
 class LoanWithRefinancing(Loan):
-    def __init__(self, refinancing_year: int, refinancing_interest: float, *args, **kwargs):
+    def __init__(
+        self,
+        refinancing_year: int,
+        refinancing_interest: float,
+        new_hypo_length_change_years: int,
+        *args,
+        **kwargs,
+    ):
         self.refinancing_year = refinancing_year
         self.refinancing_interest = refinancing_interest
+        self.new_hypo_length_change_years = new_hypo_length_change_years
         super().__init__(*args, **kwargs)
 
-    def _amortize(self):
-        initialize = Installment(
-            number=0,
-            payment=0,
-            interest=0,
-            principal=0,
-            total_interest=0,
-            balance=self.principal,
-        )
-        schedule = [initialize]
-        total_interest = 0
-        balance = self.principal
-        for payment_number in range(1, self.term * self.n_periods + 1):
-
+    def schedule_with_refinancing(self) -> list[namedtuple]:
+        new_schedule = []
+        for part in self.schedule():
+            new_schedule.append(part)
             # Refinancing
-            if payment_number == self.refinancing_year * self.n_periods:
-                self.interest = Decimal(self.refinancing_interest * 100) / 100
-                balance = schedule[-1].balance
-                total_interest = 0
-
-            split = self.split_payment(payment_number, self._monthly_payment)
-            interest_payment, principal_payment = split
-
-            total_interest += interest_payment
-            balance -= principal_payment
-            installment = Installment(
-                number=payment_number,
-                payment=self._monthly_payment,
-                interest=interest_payment,
-                principal=principal_payment,
-                total_interest=total_interest,
-                balance=balance,
-            )
-
-            schedule.append(installment)
-
-        return schedule
+            if part.number == self.n_periods * self.refinancing_year:
+                new_loan = Loan(
+                    principal=part.balance,
+                    interest=self.refinancing_interest,
+                    term=self.term - self.refinancing_year + self.new_hypo_length_change_years,
+                )
+                break
 
 
-# Example
-# loan = LoanWithRefinancing(principal=hypo_total, interest=p_a, term=years, currency="kč", refinancing_year=refinancing_year, refinancing_interest=0.06)
+        for part in new_loan.schedule():
+            print(part)
+            new_schedule.append(part)
 
-# loan.summarize
+        return new_schedule
 
-# for part in loan.schedule():
-#    print(part)
+
+def main():
+    # Example
+    if 1:
+        hypo_total = 100_000
+        p_a = 0.02
+        years = 2
+        refinancing_year = 1
+        refinancing_p_a = 0.1
+        length_change = 0
+
+    if 0:
+        hypo_total = 200_000
+        p_a = 0.02
+        years = 2
+        refinancing_year = 1
+        refinancing_p_a = 0.1
+        length_change = 0
+
+    if 0:
+        hypo_total = 2_500_000
+        p_a = 0.0169
+        years = 30
+        refinancing_year = 7
+        refinancing_p_a = 0.0564
+        length_change = -7
+
+    loan = LoanWithRefinancing(
+        principal=hypo_total,
+        interest=p_a,
+        term=years,
+        refinancing_year=refinancing_year,
+        refinancing_interest=refinancing_p_a,
+        new_hypo_length_change_years=length_change,
+    )
+
+    total_interest = 0
+    total_principal = 0
+
+    for part in loan.schedule_with_refinancing():
+        print(part)
+        total_interest += part.interest
+        total_principal += part.principal
+
+    print(f"Total interest: {total_interest}")
+    print(f"Total principal: {total_principal}")
+
+
+if __name__ == "__main__":
+    main()
