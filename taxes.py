0b7bfa37037edea83297ba5b432ded9ad989af4b
Tomas Holecek <holecek.to@gmail.com>
2024-11-23 13:46:59 +0100

    apply only 15% as discount

--- taxes.py
+++ taxes.py
@@ -1,17 +1,16 @@
-
 class Taxes:
 
     def __init__(self, interests_paid: list, max_discount: int = 150000):
-        self.interests_paid: list = interests_paid
+        self.tax_discounts: list = [i * 0.15 for i in interests_paid]
         self.max_discount = max_discount
         self.interests_yearly: list = []
         self.discounts: list = []
-    
+
     def aggregate_yearly_values(self):
         months_in_year = 12
-        for i in range(0, len(self.interests_paid), months_in_year):
-            yearly_sum = sum(self.interests_paid[i:i+months_in_year])
-            self.interests_yearly.append(yearly_sum)
+        for i in range(0, len(self.tax_discounts), months_in_year):
+            yearly_sum = sum(self.tax_discounts[i : i + months_in_year])
+            self.interests_yearly.append(round(yearly_sum, 2))
 
     def calculate_discounts(self):
         if not self.interests_yearly:
@@ -23,10 +22,11 @@ class Taxes:
                 self.discounts.append(i)
         return self.discounts
 
+
 if __name__ == "__main__":
     interests = []
     for i in range(120):
         interests.append(i)
-    taxes = Taxes(interests, 1000)
+    taxes = Taxes(interests, 100)
     discounts = taxes.calculate_discounts()
     print(discounts)
