42d6466fcf3e3560beed6b858c1b4677e9402181
Vojtěch Oram <vojtech@leadspicker.com>
2024-11-23 16:43:18 +0100

    main reformat

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import math
+import streamlit as st
 
-from investor import InvestmentData, Investor
 from loan_with_refinancing import LoanWithRefinancing
 
-st.title("Kalkulacka refinancovani hypoteky")
+st.title("Kalkulačka refinancování hypotéky")
 
 #########################################
 #                                       #
@@ -14,14 +11,13 @@ st.title("Kalkulacka refinancovani hypoteky")
 #                                       #
 #########################################
 
-st.write("### Vstupni data - hypoteka")
 col1, col2 = st.columns(2)
-home_value = col1.number_input("Hypotecni pujcka", min_value=0, value=100_000)
-old_interest_rate = col2.number_input("Urok [%]", min_value=0.1, value=1.0) / 100
-old_loan_length = col1.number_input("Doba splaceni [roky]", min_value=1, value=30)
-old_loan_current_year = col1.number_input("Rok refinancovani", min_value=1, max_value=old_loan_length - 1, value=1)
-refinancing_interest_rate = col2.number_input("Urok refinancovani [%]", min_value=0.1, value=5.0) / 100
-new_loan_length = col2.number_input("Doba refinancovani (v letech)", min_value=0, value=7)
+home_value = col1.number_input("Původní splácená částka", min_value=0, value=100_000)
+old_interest_rate = col2.number_input("Původní úrok [%]", min_value=0.1, value=2.0) / 100
+old_loan_length = col1.number_input("Původní délka splácení [roky]", min_value=1, value=20)
+old_loan_current_year = col1.number_input("Rok refinancování", min_value=1, max_value=old_loan_length - 1, value=5)
+refinancing_interest_rate = col2.number_input("Úrok po refinancování [%]", min_value=0.1, value=5.0) / 100
+new_loan_length = col2.number_input("Relativní prodloužení/zkrácení hypotéky po refinancování (v letech)", min_value=-20, value=7)
 length_change = old_loan_length - new_loan_length
 
 risk_choice = st.selectbox(
@@ -31,24 +27,26 @@ risk_choice = st.selectbox(
 
 
 loan = LoanWithRefinancing(
-    principal = home_value,
-        interest = old_interest_rate,
-        term = old_loan_length,
-        refinancing_year = old_loan_current_year,
-        refinancing_interest = refinancing_interest_rate,
-        new_hypo_length_change_years = length_change,
+    principal=home_value,
+    interest=old_interest_rate,
+    term=old_loan_length,
+    refinancing_year=old_loan_current_year,
+    refinancing_interest=refinancing_interest_rate,
+    new_hypo_length_change_years=length_change,
 )
 data = []
 for inst in loan.schedule_with_refinancing():
-    data.append({
-        'number': inst.number,
-        'payment': float(inst.payment),
-        'interest': float(inst.interest),
-        'principal': float(inst.principal),
-        'total_interest': float(inst.total_interest),
-        'balance': float(inst.balance),
-        "investment_values": float(inst.investment_values.get(risk_choice, 0)),
-    })
+    data.append(
+        {
+            "number": inst.number,
+            "payment": float(inst.payment),
+            "interest": float(inst.interest),
+            "principal": float(inst.principal),
+            "total_interest": float(inst.total_interest),
+            "balance": float(inst.balance),
+            "investment_values": float(inst.investment_values.get(risk_choice, 0)),
+        }
+    )
 
 df = pd.DataFrame(data)
 
@@ -61,4 +59,4 @@ investment_data = df["investment_values"]
 st.line_chart(overpayment_df)
 st.line_chart(payment_details_df)
 with st.expander("Zdrojova data"):
-	st.table(df)
+    st.table(df)
